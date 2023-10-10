83ec366a4bbe20497af0dd64988fb101e04661e1
Julia Chelnokova <chelnokova.yuv@yandex.ru>
2023-10-10 23:32:33 +0700

    изменение порта

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorHandler = require('./middlewares/error-handler');
 const routers = require('./routes');
 const limiter = require('./utils/limiter');
 
-const { PORT = 3000, DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 4000, DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
